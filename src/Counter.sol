b91a63be659b3abf6d92e43bf447c5cd9b8b2b69
ritik-agarwal50 <ritikagarwal570@gmail.com>
2024-10-16 13:38:52 +0530

    complete

--- src/Counter.sol
+++ src/Counter.sol
@@ -14,8 +14,39 @@ contract Counter {
     }
 }
 
+//NORMAL TESTING
 contract TestCounter is Counter {
     function echidna_test_pass() public pure returns (bool) {
         return true;
     }
+
+    function echidna_test_count() public view returns (bool) {
+        return count <= 9;
+    }
+}
+
+//ASSERTION TESTING
+contract TestAssert is Counter {
+    //this will fail
+    function test_assert(uint _c) external pure {
+        assert(_c <= 9);
+    }
+
+    function abs(uint x, uint y) private pure returns(uint) {
+        if( x >= y){
+            return x - y;
+        } 
+        return y - x;
+    }
+
+    //This shoudl pass 
+    function test_abs(uint x, uint y) external pure {
+        uint z = abs(x, y);
+        if(x >= y){
+            assert(z <=x);
+        } else {
+            assert(z <= y);
+        }
+    }
+
 }
